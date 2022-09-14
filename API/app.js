a5014bb473879fc147358f56d58f59c45245c7cc
Robson Calixto <robsoncaliixto@gmail.com>
2022-09-14 19:56:27 -0300

    fix: add initial route on express

--- API/app.js
+++ API/app.js
@@ -1,15 +1,20 @@
-import express from "express";
+const express = require('express');
 const app = express();
+const port = '8080';
 
-const sequelize = new Sequelize('sqlite::memory:');
-
+/*const sequelize = new Sequelize('sqlite::memory:');
 try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
+*/
 
-app.listen(3452, function () {
-    console.log("link: http://localhost:3452");
+app.get('/', (req, res) => {
+  res.send('My first route');
 })
+
+app.listen(port,  () => {
+  console.log(`app listening on port:${port}`);
+});
