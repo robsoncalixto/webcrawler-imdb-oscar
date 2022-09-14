b92733c6a6bc9b9c688802771d0f04453fdfe700
Robson Calixto <robsoncaliixto@gmail.com>
2022-09-14 19:45:57 -0300

    fix: changed readme and main flow of python

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# A simple webcrawler to access IMDB
+# IMDB webcrawler
+The project goal is to navegate on IMDB list Oscar 2022 using selenium and others tecnology that will allow us to save in a database, to show on the website from an api rest.
 
 ## How to use
+
+### install depencies of python
+- pip      22.2.1
+- selenium 4.3.0
+- Python   3.10.5
+
+## Running
+```python
+### Run webcrawler
+py ./webcrawler/Automacao.py
+```
--- webcrawler/Automacao.py
+++ webcrawler/Automacao.py
@@ -1,14 +1,14 @@
 #import pyautogui
 #import time
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import service
+#from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
+#from selenium.webdriver.chrome.service import service
 
-servico = (ChromeDriveManager().install)
-navegador = webdriver.chrome(service=servico)
+#servico = (ChromeDriveManager().install)
+#navegador = webdriver.chrome(service=servico)
 
-navegador.get("https://www.imdb.com/")
-navegador.find_element('xpath', '//*[@id="iconContext-menu"]/path[2]').click()
+#navegador.get("https://www.imdb.com/")
+#navegador.find_element('xpath', '//*[@id="iconContext-menu"]/path[2]').click()
 
 #pyautogui.alert("O código vai começar. Não use nada do seu computador enquanto o código está executando")
 #pyautogui.PAUSE = 0.5
@@ -22,3 +22,9 @@ navegador.find_element('xpath', '//*[@id="iconContext-menu"]/path[2]').click()
 #pyautogui.write("https://www.imdb.com/")
 #pyautogui.press('enter')
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+browser = webdriver.Chrome(service=Service("webcrawler\\driver\\chromedriver.exe") )
+browser.get("https://www.imdb.com/oscars/?ref_=nv_ev_acd")
+browser.find_element('xpath',"//a[contains(text(),'2022')]").click()
